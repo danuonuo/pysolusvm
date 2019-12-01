05bab154db370900c707728b2149e77d16c4a291
Nathan Lopez <contact@lopeznathan.com>
2019-11-30 22:13:14 -0600

    setup.py Added

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name='pysolusvm',
+    version='1.0',
+    author='Nathan Lopez',
+    author_email='contact@lopeznathan.com',
+    description='Python SolusVM API',
+    url='https://github.com/LopezNathan/pysolusvm',
+    packages=['pysolusvm'],
+    python_requires='>=3.6',
+    install_requires=['requests'],
+)
